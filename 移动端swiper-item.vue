36f362152a0ec55e45c1a02c326574aa689b69a8
求职 有意者查看简历 https://github.com/a62527776a/jianli/blob/master/readme.md <1803111202@qq.com>
2018-03-22 17:35:14 +0800

    Create 移动端swiper-item.vue

---
+++ 移动端swiper-item.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+  .rr-swiper-item
+    slot
+</template>
+
+<script>
+export default {
+  name: 'rr-swiper-item',
+  props: {
+    idx: {
+      type: Number,
+      default: 0
+    }
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  created () {
+    console.log(this.idx)
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.rr-swiper-item
+  position: absolute;
+  width: 100%;
+  background: red;
+</style>
